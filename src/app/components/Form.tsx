14f578bf5fb6d3967a8aab46649fb34c3219af13
Mory <mory.bamba1@gmail.com>
2023-05-19 04:42:24 +0200

    Add an URL checker

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -1,10 +1,31 @@
-import React from "react";
+import { ChangeEvent } from "react";
 
-const Form = () => {
+type Props = {
+  url: string;
+  setUrl: (url: string) => void;
+};
+
+const Form = ({ url, setUrl }: Props) => {
+  const onChange = (event: ChangeEvent<HTMLInputElement>) =>
+    setUrl(event.target.value);
+
   return (
     <form className="pt-12 w-full flex">
-      <input type="text" name="url" className="w-3/4 h-12 pl-4 text-black mr-8 rounded-md"/>
-      <button type="submit" className="bg-bgButton text-black font-bold w-1/4 rounded-md">Raccourcir</button>
+      <input
+        type="text"
+        name="url"
+        value={url}
+        onChange={onChange}
+        placeholder="Entre votre url (https://...)"
+        className="w-3/4 h-12 pl-4 text-black mr-8 rounded-md"
+      />
+      <button
+        type="submit"
+        className="bg-bgButton text-black font-bold w-1/4 rounded-md"
+        disabled={true}
+      >
+        Raccourcir
+      </button>
     </form>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,55 @@
+"use client";
+
+import React, { useEffect, useMemo, useState } from "react";
+import debounce from "lodash.debounce";
 import Alert from "./components/Alert";
 import Form from "./components/Form";
 
 export default function Home() {
+  const [url, setUrl] = useState("");
+  const [isUrlValid, setIsUrlValid] = useState<boolean>();
+
+  const alert = () => {
+    if (url === "") {
+      return null;
+    }
+
+    if (isUrlValid === false) {
+      return <Alert message="L'url saisie est invalide" type="error" />;
+    }
+
+    return <Alert message="L'url saisie est valide" type="success" />;
+  };
+
+  const ping = useMemo(
+    () =>
+      debounce(async (url: string) => {
+        try {
+          const response = await fetch(url, {
+            method: "HEAD",
+            mode: "no-cors",
+          });
+          setIsUrlValid(response.status === 200 || response.status === 0);
+        } catch (error) {
+          setIsUrlValid(false);
+        }
+      }, 500),
+    []
+  );
+
+  useEffect(() => {
+    if (url === "") {
+      return;
+    }
+
+    ping(url);
+  }, [url, ping]);
+
   return (
     <>
-      <Form />
+      <Form url={url} setUrl={setUrl} />
       <hr className="w-full h-1 mt-12 mb-12 bg-colorHr border-none shadow" />
-      <Alert message="L'url saisie est invalide" type="error" />
+      {alert()}
     </>
   );
 }
